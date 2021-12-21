14b5eae904d4fee2fa0981437d21d8b682a46e10
FaysalAmin77 <95224798+FaysalAmin77@users.noreply.github.com>
2021-12-20 20:36:44 -0800

    Add files via upload

---
+++ Main (2).java
@@ -0,0 +1,20 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	
+		int[] arrayNumbr={1,23,4,6,7,8};
+		System.out.println(arrayNumbr[3]);
+		int arraysum;
+		
+		int[][] twodarray=new int[2][2];
+		twodarray[0][0]=1;
+		twodarray[0][1]=2;
+		twodarray[1][0]=3;
+		twodarray[1][1]=4;
+		
+		arraysum= twodarray[0][0]+twodarray[1][1];
+		System.out.println(arraysum);
+	    
+	}
+}
